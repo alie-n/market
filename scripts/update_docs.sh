5e493cef39421d135245d46fe9f0208dad1ac346
ali-n <redmouse@mailbox.org>
2021-03-11 21:08:14 +0300

    fix: incorrect link to the script file in travis
    change link

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -2,9 +2,6 @@
 
 if ["`git show -s --format='%ae'`" -ne "travis@travis-ci.org"];
 then
-    cd $HOME
-
-    git clone --quiet --branch=master https://$GITHUB_TOKEN@github.com/alie-n/market.git market
 
     rm -r $HOME/market/docs/apidocs
     cp -r $HOME/build/alie-n/market/target/site/apidocs $HOME/market/docs/
@@ -15,5 +12,4 @@ then
     git commit -m "update javadoc. build $TRAVIS_BUILD_NUMBER"
     git push https://$GITHUB_TOKEN@github.com/alie-n/market master
 
-    rm -rf $HOME/market
 fi
