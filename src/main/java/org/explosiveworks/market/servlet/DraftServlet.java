6e549a6da3540aac8c7a1a8e6109c7c532e70775
xxcw <explosiveworks@protonmail.com>
2021-03-16 13:55:36 +0300

    feat: switch to get request supporting
    add doGet method

--- src/main/java/org/explosiveworks/market/servlet/DraftServlet.java
+++ src/main/java/org/explosiveworks/market/servlet/DraftServlet.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 public class DraftServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request,
+    protected void doGet(HttpServletRequest request,
                           HttpServletResponse response)
             throws ServletException, IOException {
 
